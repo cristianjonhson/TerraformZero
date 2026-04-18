8d5475727de34238bbd94ea33c5ddba2704f33db
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-18 13:57:06 -0400

    feat: Agregar comentario descriptivo en output.tf para mayor claridad

--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,4 @@
+# Outputs para validar rapidamente que el recurso local_file se creo correctamente.
 output "generated_file_path" {
   description = "Ruta del archivo generado por el recurso local_file."
   value       = local_file.example.filename
