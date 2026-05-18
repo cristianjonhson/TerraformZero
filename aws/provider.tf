0d27219bf5ce93778f78bce3a2ead42b551fca41
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 11:53:16 -0400

    feat: se utilizan variable de acceso de prueba

--- aws/provider.tf
+++ aws/provider.tf
@@ -12,4 +12,7 @@ terraform {
 provider "aws" {
   #  Configuración del proveedor AWS, como región o credenciales, puede ser definida aquí o a través de variables de entorno.
   region = var.aws_region
+  #provider para conectarse a ministack, se recomienda usar variables de entorno o un gestor de secretos para las claves.
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
