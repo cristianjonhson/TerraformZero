9c74110bb4526480bc638c6d0ec9f2851424835c
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 11:52:52 -0400

    feat: se crean variables de acceso como prueba para conectar

--- aws/variables.tf
+++ aws/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "aws_access_key" {
+  description = "Clave de acceso para AWS, se recomienda usar variables de entorno o un gestor de secretos."
+  type        = string
+  default     = "test"
+}
+
+variable "aws_secret_key" {
+  description = "Clave secreta para acceder a AWS, se recomienda usar variables de entorno o un gestor de secretos."
+  type        = string
+  default     = "test"
+}
