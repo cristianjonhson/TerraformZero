23f559134454ddea3648b50f25ac7fdc262ddb44
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 12:22:26 -0400

    feat: se crea salida para la Region del s3

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -3,6 +3,11 @@ output "bucket_arn" {
   value       = aws_s3_bucket.example.arn
 }
 
+output "bucket_region" {
+  description = "Región donde se creó el bucket de S3."
+  value       = aws_s3_bucket.example.region
+}
+
 output "aws_s3_bucket_name" {
   description = "Nombre del bucket de S3 creado por Terraform."
   value       = aws_s3_bucket.example.bucket
