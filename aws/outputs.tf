54fef850379969ced2c59802cd98183b5fbe3a24
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 12:21:58 -0400

    feat: se crea salida para el ARN del s3

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,3 +1,8 @@
+output "bucket_arn" {
+  description = "ARN del bucket de S3 creado por Terraform."
+  value       = aws_s3_bucket.example.arn
+}
+
 output "aws_s3_bucket_name" {
   description = "Nombre del bucket de S3 creado por Terraform."
   value       = aws_s3_bucket.example.bucket
