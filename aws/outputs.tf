019bce1984ed09c1fe1a4249c0f27f252d02285e
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 12:22:47 -0400

    feat: se crea salida para el id  del s3

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -8,6 +8,11 @@ output "bucket_region" {
   value       = aws_s3_bucket.example.region
 }
 
+output "bucket_id" {
+  description = "ID del bucket de S3 creado por Terraform."
+  value       = aws_s3_bucket.example.id
+}
+
 output "aws_s3_bucket_name" {
   description = "Nombre del bucket de S3 creado por Terraform."
   value       = aws_s3_bucket.example.bucket
