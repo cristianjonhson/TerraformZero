e6592fbb37fc74b575ffec7217590650eb253b65
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-18 12:19:57 -0400

    fix: se arregla la nomenclatura de variables para el nombre del bucket

--- aws/main.tf
+++ aws/main.tf
@@ -1,6 +1,6 @@
 # Recurso de Terraform para crear un bucket de S3 en AWS. El nombre del bucket es único gracias a la función random_pet que genera un sufijo aleatorio.
 resource "aws_s3_bucket" "example" {
-  bucket = var.aws_s3_bucket_name
+  bucket = "${var.env}-${var.aws_s3_bucket_name}"
 
   tags = {
     Name        = var.aws_s3_bucket_name
