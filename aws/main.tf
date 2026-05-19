f6eff85d0fd8a15643f9f94af721f66c46bb8c0a
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-19 12:31:17 -0400

    fix: se arregla la composicion de variables usando un locals para el nombre y tag del s3

--- aws/main.tf
+++ aws/main.tf
@@ -1,9 +1,17 @@
+resource "random_pet" "project_suffix" {
+  length = var.random_pet_length
+}
+
+locals {
+  bucket_name = lower("${var.env}-${var.aws_s3_bucket_name}-${random_pet.project_suffix.id}")
+}
+
 # Recurso de Terraform para crear un bucket de S3 en AWS. El nombre del bucket es único gracias a la función random_pet que genera un sufijo aleatorio.
 resource "aws_s3_bucket" "example" {
-  bucket = "${var.env}-${var.aws_s3_bucket_name}"
+  bucket = local.bucket_name
 
   tags = {
-    Name        = var.aws_s3_bucket_name
+    Name        = local.bucket_name
     Environment = var.env
   }
 }
