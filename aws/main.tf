22621001973dfaae73a19556bd8fa6fb708708dc
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-19 13:13:17 -0400

    fix: se separa el bucket de su tagging usando un recurso dedicado.

--- aws/main.tf
+++ aws/main.tf
@@ -13,13 +13,27 @@ locals {
     ManagedBy   = var.managed_by
     Component   = var.component_name_tag_value
   }
+
+  bucket_tags = merge(local.common_tags, {
+    Name = local.bucket_name
+  })
 }
 
 # Recurso de Terraform para crear un bucket de S3 en AWS. El nombre del bucket es único gracias a la función random_pet que genera un sufijo aleatorio.
 resource "aws_s3_bucket" "example" {
   bucket = local.bucket_name
+}
 
-  tags = merge(local.common_tags, {
-    Name = local.bucket_name
-  })
+# Recurso de Terraform para aplicar etiquetas al bucket de S3 creado. Las etiquetas se definen en la variable local.bucket_tags, que combina etiquetas comunes con una etiqueta específica para el nombre del bucket.
+resource "aws_s3_bucket_tagging" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  dynamic "tag_set" {
+    for_each = local.bucket_tags
+
+    content {
+      key   = tag_set.key
+      value = tag_set.value
+    }
+  }
 }
